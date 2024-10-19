8d19a775667833e936c725802333e908107cc5ea
Nathan Wong <natdanwongielhan@yahoo.com>
2024-10-19 15:14:59 -0700

    add text to header and other files

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,15 @@
+import "./Header.css";
+import Tab from "./Tab";
+
+function Header() {
+  // Headers: Top, About Me, Projects, Contact Me
+  return (
+    <div className="header">
+      <div className="introText">
+        Hi, I'm Nathan Wong, a full stack developer. Welcome to my page
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ portfolio/src/components/Tab.js
@@ -0,0 +1,7 @@
+import "./Tab.css";
+
+function Tab(props) {
+  return <div className="tab">{props.tabName}</div>;
+}
+
+export default Tab;
