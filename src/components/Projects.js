4d35c720ae3d74d0fd955cf8dbd6865046a87882
ndw001 <natdanwongielhan@yahoo.com>
2024-11-14 20:22:31 -0800

    clean up Projects placeholder

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,17 @@ function Projects() {
 
   const testProjects = ["Set"];
 
+  // alternating Left and Right Text and Picture
+  // Text box is CLAIM: Caption
+
   return (
     <div className="projects">
       {testProjects.map((tab, index) => (
-        <div key={index}>{tab}</div>
+        <div className="projectContainer" key={index}>
+          <div className="title"> {tab} </div>
+          <div className="textBox"> Text Box </div>
+          <div className="photo"> Photo </div>
+        </div>
       ))}
     </div>
   );
