4bf35a043f3e18beb57cc4408c4934e4deb411fd
ndw001 <natdanwongielhan@yahoo.com>
2024-11-14 19:33:10 -0800

    add placeholder for Projects and Contact Me section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import IntroPage from "./components/IntroPage";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <IntroPage />
       <Header />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import "./Contact.css";
+
+function Contact() {
+  // Center in Middle of page??
+  // Picture ??
+  //
+
+  return (
+    <div className="contact">
+      <div className=" "> Contact Me! </div>
+      <div className="name"> Nathan Wong </div>
+      <div className="number"> Phone Number: (626)319-5118 </div>
+      <div className="email"> Email: natdanwongielhan@yahoo.com </div>
+      <div className="location"> Los Angeles, CA, USA </div>
+    </div>
+  );
+}
+
+export default Contact;
