a2f0909e2765e1c4800aa71068d94177f9252653
ndw001 <natdanwongielhan@yahoo.com>
2024-11-18 21:15:20 -0800

    added description text and centered it

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -4,8 +4,20 @@ function IntroPage() {
   // Headers: Top, About Me, Projects, Contact Me
   return (
     <div className="introPage">
-      <div className="introText">
-        Hi, I'm Nathan Wong, a full stack developer. Welcome to my page!
+      <div className="welcomeText">
+        Hi, I'm Nathan Wong. Welcome to my page!
+      </div>
+      <div className="descriptionText">
+        I'm a passionate software developer dedicated to crafting innovative and
+        efficient solutions that solve real-world problems. With a strong
+        foundation in JavaScript and React, I specialize in building intuitive
+        user interfaces, as well as backend processes for processing data. I
+        thrive in collaborative environments, constantly seeking opportunities
+        to learn, grow, and create impactful projects.
+        <br />
+        Feel free to explore my portfolio to see the projects I’ve worked on,
+        showcasing my skills in coding, design, and problem-solving. Let’s build
+        something great together!
       </div>
     </div>
   );
