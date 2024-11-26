dbe4bede2ad5640ca1a461aeab9846b7f46d0bb6
ndw001 <natdanwongielhan@yahoo.com>
2024-11-25 16:19:34 -0800

    add Projects header

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,17 +3,27 @@ import "./Projects.css";
 function Projects() {
   // Each Section is a different Project
 
-  const testProjects = ["Set"];
+  // const testProjects = ["Set"];
+
+  const testProjects = [
+    {
+      name: "Set",
+      descTitle: "Card game",
+      desc: "This is a game",
+      photo: "",
+    },
+  ];
 
   // alternating Left and Right Text and Picture
   // Text box is CLAIM: Caption
 
   return (
     <section id="projects">
+      <header className="projectsHeader">Projects</header>
       <div className="projects">
         {testProjects.map((tab, index) => (
           <div className="projectContainer" key={index}>
-            <div className="title"> {tab} </div>
+            <div className="title"> {tab.name} </div>
             <div className="textBox"> Text Box </div>
             <div className="photo"> Photo </div>
           </div>
